7c4e7a1466727582def09c157b23af4f4e8e80fd
Edmond Lee <techhexium@gmail.com>
2020-02-12 22:58:46 -0800

    Refactor Caltrans shapefile seed script

--- db/fixtures.js
+++ db/fixtures.js
@@ -1,70 +1,49 @@
-// Populate the database with GIS data.
-// Parse the shapefile to GeoJSON, then extract data for the state, route, and coordinates for each route.
 const fs = require('fs').promises;
 const shapefile = require('shapefile');
 const Utils = require('./Utils.js');
 
-const CA_DATA = 'db/ca.shp', CA_DB = 'db/ca.dbf';
+const CA_DATA = 'db/SHN_Lines.shp', CA_DB = 'db/SHN_Lines.dbf';
 const STATES = 'states', ROUTES = 'routes', POINTS = 'points';
 
 const seedData = async (db) => {
-  let features = await shapefile.read(CA_DATA, CA_DB).then(collection => collection.features);
-  let stateKey = await db.queryAsync(`INSERT INTO ${STATES} (name, initials) VALUES ("California", "CA")`).then(res => res[0].insertId);
-  let base = 0;
+  const features = await shapefile.read(CA_DATA, CA_DB).then(collection => collection.features);
+  const stateID = await db.queryAsync(`INSERT INTO ${STATES} (name, initials) VALUES ("California", "CA")`).then(res => res[0].insertId);
+  let basePointID = 0;
 
+  // Can't use Promise.all as we need to insert synchronously
   for (let feature of features) {
-    let route = {
-      num: `'${feature.properties.ROUTE}'`,
-      dir: `'${feature.properties.DIR}'`,
-      seg: 0
-    };
-
+    const routeNum = `'${feature.properties.ROUTE}'`;
+    const dir = `'${feature.properties.DIR}'`;
     // The curve is either in a single array or multiple arrays
-    let isSingleCurve = feature.geometry.type === 'LineString';
-
-    // Insert each array as its own route. Also calculate the length in meters of the segment
-    if (!isSingleCurve) {
-      for (let i = 0; i < feature.geometry.coordinates.length; i++) {
+    if (feature.geometry.type !== 'LineString') {
+      for (let i = 0; i < feature.geometry.coordinates.length; i += 1) {
         const len = feature.geometry.coordinates[i].length;
-        let num = await db.queryAsync(`INSERT INTO ${ROUTES} (route, segment, direction, state_key, len, base) VALUES (${route.num}, ${route.seg}, ${route.dir}, ${stateKey}, ${len}, ${base});`).then(res => res[0].insertId);
-
-        let temp = feature.geometry.coordinates[i].map(tup => {
-          return { route: num, lat: tup[1], lon: tup[0] };
-        });
-        temp = temp.map(obj => `(${obj.route}, ${obj.lat}, ${obj.lon})`);
-        const len_meters = Utils.calcSegmentDistance(feature.geometry.coordinates[i].map(tup => [tup[1], tup[0]]));
-
-        await db.queryAsync(`UPDATE ${ROUTES} SET len_m = ${len_meters} WHERE id = ${num};`);
-        await db.queryAsync(`INSERT INTO ${POINTS} (route_key, lat, lon) VALUES ${temp.join()};`);
-
-        console.log(`Seeded ${temp.length} points`);
-        route.seg += 1;
-        base += temp.length;
+        const routeID = await db.queryAsync(`INSERT INTO ${ROUTES} (route, segment, direction, state_key, len, base) VALUES (${routeNum}, ${i}, ${dir}, ${stateID}, ${len}, ${basePointID});`).then(res => res[0].insertId);
+        await processCoordinates(db, routeID, feature.geometry.coordinates[i]);
+        basePointID += len;
       }
     } else {
       const len = feature.geometry.coordinates.length;
-      let num = await db.queryAsync(`INSERT INTO ${ROUTES} (route, segment, direction, state_key, len, base) VALUES (${route.num}, ${route.seg}, ${route.dir}, ${stateKey}, ${len}, ${base});`).then(res => res[0].insertId);
-      let newPoints = feature.geometry.coordinates.map(tup => {
-        return { route: num, lat: tup[1], lon: tup[0] };
-      });
-      newPoints = newPoints.map(obj => `(${obj.route}, ${obj.lat}, ${obj.lon})`);
-      const len_meters = Utils.calcSegmentDistance(feature.geometry.coordinates.map(tup => [tup[1], tup[0]]));
-
-      // Insert all rows by using commas
-      await db.queryAsync(`UPDATE ${ROUTES} SET len_m = ${len_meters} WHERE id = ${num};`);
-      await db.queryAsync(`INSERT INTO ${POINTS} (route_key, lat, lon) VALUES ${newPoints.join()};`);
-
-      console.log(`Seeded ${newPoints.length} points`);
-      base += newPoints.length;
+      const routeID = await db.queryAsync(`INSERT INTO ${ROUTES} (route, segment, direction, state_key, len, base) VALUES (${routeNum}, 0, ${dir}, ${stateID}, ${len}, ${basePointID});`).then(res => res[0].insertId);
+      await processCoordinates(db, routeID, feature.geometry.coordinates);
+      basePointID += len;
     }
   }
 
   console.log('Creating indices...');
-  return db.queryAsync('CREATE INDEX POINT_IDX ON points (route_key);')
-    .then(res => db.queryAsync('CREATE INDEX ROUTE_IDX on routes (route(4), direction(1));'));
+  return db.queryAsync(`CREATE INDEX POINT_IDX ON ${POINTS} (route_key);`)
+    .then(res => db.queryAsync(`CREATE INDEX ROUTE_IDX on ${ROUTES} (route(4), direction(1));`));
 };
+
+const processCoordinates = async (db, routeID, coords) => {
+  const items = coords.map(tup => [routeID, tup[1], tup[0]]);
+  const lenInMeters = Utils.calcSegmentDistance(coords.map(tup => [tup[1], tup[0]]));
+  await db.queryAsync(`UPDATE ${ROUTES} SET len_m = ${lenInMeters} WHERE id = ${routeID};`);
+  await db.queryAsync(`INSERT INTO ${POINTS} (route_key, lat, lon) VALUES ?;`, [items]);
+  console.log(`Seeded route with ID ${routeID} with ${items.length} points, length = ${lenInMeters}m`);
+}
 
 // Check if the database is empty before populating it with mock data.
 // Remember that the results will be a 2D array, first element has actual results
-module.exports = db => db.queryAsync('SELECT * FROM states')
+module.exports = db => db.queryAsync(`SELECT * FROM ${STATES}`)
   .then(results => (results[0].length === 0 ? seedData(db) : undefined));
