d584cb2c7d8dc3188c87cd2efee1581834a3fd62
Edmond Lee <techhexium@gmail.com>
2021-07-18 23:35:42 -0700

    Refactor Sidebar logic to use react-router Use hash routing, some metadata, and readded listeners

--- client/components/RouteDrawer.tsx
+++ client/components/RouteDrawer.tsx
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
 import {
-  Info, Map, Search, User,
+  ArrowLeft, Info, Map, Search, User,
 } from 'react-feather';
+import {
+  HashRouter, Link, Route, Switch, useLocation,
+} from 'react-router-dom';
 import type { IHighways } from '../types/interfaces';
 import type {
   RouteDataCallbackMap,
@@ -13,8 +16,8 @@ import type {
 import AboutContent from './AboutContent';
 import SearchResults from './SearchResults';
 import RouteSegmentContent from './RouteSegmentContent';
-import Sidebar from './Sidebar';
-import SidebarTab from './SidebarTab';
+// import Sidebar from './Sidebar';
+// import SidebarTab from './SidebarTab';
 import TravelSegmentContent from './TravelSegmentContent';
 
 const ICON_SIZE = 16;
@@ -36,55 +39,126 @@ const RouteDrawer = ({
 }: Props): React.ReactElement<Props> => {
   const { routeSegments, stateId } = routeData;
   const [isCollapsed, setCollapsed] = useState<boolean>(false);
-  const [selectedId, setSelectedId] = useState<string>('routeSegments');
+  const { hash } = useLocation();
 
-  const _onClose = (): void => {
+  const _onClose = () => {
     setCollapsed(true);
   };
 
-  const _onToggle = (id: string) => {
-    if (selectedId === id) {
+  const _onToggle = (activeHash: string) => {
+    if (hash === activeHash) {
       setCollapsed(!isCollapsed);
     }
     if (isCollapsed) {
       setCollapsed(false);
     }
-    setSelectedId(id);
   };
 
-  return (
-    <Sidebar
-      id="sidebar"
-      collapsed={isCollapsed}
-      selected={selectedId}
-      onClose={_onClose}
-      onToggle={_onToggle}
-    >
-      <SidebarTab id="users" header="User Settings" icon={<User size={ICON_SIZE} />}>
+  const collapsedClass = isCollapsed ? 'collapsed' : '';
+
+  const routes = [
+    {
+      Content: () => (
         <TravelSegmentContent
           highwayData={highwayData}
           userData={userData}
           userDataCallbackMap={userDataCallbackMap}
         />
-      </SidebarTab>
-      <SidebarTab id="routeSegments" header="Route Segments" icon={<Map size={ICON_SIZE} />}>
+      ),
+      Icon: () => <User size={ICON_SIZE} />,
+      activeHash: '#travel_segments',
+      header: 'Travel Segments',
+      path: '/travel_segments',
+    },
+    {
+      Content: () => (
         <RouteSegmentContent
           highwayData={highwayData}
           routeData={routeData}
           routeDataCallbackMap={routeDataCallbackMap}
         />
-      </SidebarTab>
-      <SidebarTab id="search" header="Search" icon={<Search size={ICON_SIZE} />}>
+      ),
+      Icon: () => <Map size={ICON_SIZE} />,
+      activeHash: '',
+      exact: true,
+      header: 'Route Segments',
+      path: '/',
+    },
+    {
+      Content: () => (
         <SearchResults
           onRouteItemClick={routeDataCallbackMap.onRouteItemClick}
           routeSegments={routeSegments}
           state={stateId != null ? highwayData.getState(stateId) : null}
         />
-      </SidebarTab>
-      <SidebarTab id="about" header="About HighwayVisualizer" icon={<Info size={ICON_SIZE} />}>
-        <AboutContent />
-      </SidebarTab>
-    </Sidebar>
+      ),
+      Icon: () => <Search size={ICON_SIZE} />,
+      activeHash: '#search',
+      header: 'Search',
+      path: '/search',
+    },
+    {
+      Content: () => <AboutContent />,
+      Icon: () => <Info size={ICON_SIZE} />,
+      activeHash: '#about',
+      header: 'About HighwayVisualizer',
+      path: '/about',
+    },
+  ];
+
+  return (
+    <HashRouter hashType="noslash">
+      <div className={`leaflet-sidebar leaflet-touch leaflet-sidebar-left ${collapsedClass}`}>
+        <div className="leaflet-sidebar-tabs">
+          <ul role="tablist">
+            {
+              routes.map(({ Icon, activeHash, path }) => (
+                <li
+                  className={hash === activeHash ? 'active' : ''}
+                  key={path}
+                  onClick={() => _onToggle(activeHash)}
+                  onKeyDown={() => _onToggle(activeHash)}
+                  role="tab"
+                >
+                  <Link to={path}><Icon /></Link>
+                </li>
+              ))
+            }
+          </ul>
+        </div>
+        <div className="leaflet-sidebar-content">
+          <Switch>
+            {
+              routes.map(({
+                Content, exact, header, path,
+              }) => (
+                <Route
+                  key={path}
+                  path={path}
+                  exact={exact}
+                >
+                  <div className="leaflet-sidebar-pane active">
+                    <h1 className="leaflet-sidebar-header">
+                      {header}
+                      <div
+                        className="leaflet-sidebar-close leaflet-sidebar-close-offset"
+                        onClick={_onClose}
+                        onKeyDown={_onClose}
+                        role="button"
+                        tabIndex={-1}
+                      >
+                        <ArrowLeft />
+                      </div>
+                    </h1>
+                    <Content />
+                  </div>
+                </Route>
+              ))
+            }
+          </Switch>
+        </div>
+      </div>
+    </HashRouter>
   );
 };
 
