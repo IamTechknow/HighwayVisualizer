e5f5884ad4337df595fa2af124710fc14348553d
Edmond Lee <techhexium@gmail.com>
2022-04-17 19:59:58 -0700

    Upgrade to react 18 !

--- client/index.tsx
+++ client/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import CreateApp from './components/App';
@@ -9,13 +9,12 @@ import 'leaflet/dist/leaflet.css';
 import './app.css';
 import 'leaflet-sidebar-v2/css/leaflet-sidebar.css';
 
-ReactDOM.render(
-  (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/*" element={<CreateApp />} />
-        <Route path="/users/:user/*" element={<UserApp />} />
-      </Routes>
-    </BrowserRouter>
-  ), document.getElementById('app'),
-);
+const root = createRoot(document.getElementById('app')!);
+root.render((
+  <BrowserRouter>
+    <Routes>
+      <Route path="/*" element={<CreateApp />} />
+      <Route path="/users/:user/*" element={<UserApp />} />
+    </Routes>
+  </BrowserRouter>
+));
